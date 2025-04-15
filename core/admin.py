79089ed824644168120cd4c2799c04a86657f807
Бронислав <atlantwindow@gmail.com>
2025-04-15 14:39:03 +0300

    add admin registersTask and TaskType

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Task, TaskType
 
-# Register your models here.
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'task_type', 'priority', 'deadline', 'is_completed')
+    list_filter = ('task_type', 'priority', 'is_completed')
+    search_fields = ('name', 'description')
+    filter_horizontal = ('assigners',)
