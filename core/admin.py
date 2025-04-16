313952df1399dc15d6417e285989698610d00799
Бронислав <atlantwindow@gmail.com>
2025-04-16 12:48:29 +0300

    edited the double quotes in the admin panel.

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from .models import Task, TaskType
 
+
 @admin.register(TaskType)
 class TaskTypeAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+    list_display = ("name",)
+    search_fields = ("name",)
+
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('name', 'task_type', 'priority', 'deadline', 'is_completed')
-    list_filter = ('task_type', 'priority', 'is_completed')
-    search_fields = ('name', 'description')
-    filter_horizontal = ('assigners',)
+    list_display = ("name", "task_type", "priority", "deadline", "is_completed")
+    list_filter = ("task_type", "priority", "is_completed")
+    search_fields = ("name", "description")
+    filter_horizontal = ("assigners",)
