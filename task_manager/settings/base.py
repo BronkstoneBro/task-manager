79aad3584eb2b7bde383c03b3d17dac5d26f5179
Бронислав <atlantwindow@gmail.com>
2025-04-17 18:44:24 +0300

    update manage.py to load environment variables and modify settings module for development; add psycopg2-binary to requirements

--- task_manager/settings/base.py
+++ task_manager/settings/base.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,11 +25,6 @@ SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY",
     "django-insecure-199nlt$2^$ft_l#&-x5_(d29!+fksii3g2^@7tj+nfp+%d@$li",
 )
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-
-ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
